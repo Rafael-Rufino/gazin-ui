dce1bf3f246ad89e34aa0ef22913e6aabb509323
rafael-rufino <rafael.rufino@gazin.com.br>
2022-11-21 22:41:21 -0300

    fix: ajust code

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,17 @@
-import typescript from "rollup-plugin-typescript2";
-/* eslint-disable import/no-anonymous-default-export */
 import del from "rollup-plugin-delete";
 import pkg from "./package.json";
-
-import { babel } from "@rollup/plugin-babel";
-
+import typescript from "@rollup/plugin-typescript";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import { terser } from "rollup-plugin-terser";
-
-const production = !process.env.ROLLUP_WATCH;
 
 export default [
   {
-    input: "src/components/index.ts",
+    input: "src/index.ts",
     output: [
       {
         file: "dist/bundle.js",
         format: "esm",
         banner: "/* eslint-disable */",
-        sourcemap: true,
       },
       { file: pkg.main, format: "cjs" },
       { file: pkg.module, format: "esm" },
@@ -27,14 +19,9 @@ export default [
     plugins: [
       resolve(),
       commonjs(),
-
       del({ targets: ["dist/*", "lib"] }),
       typescript(),
-      babel({
-        babelHelpers: "bundled",
-        exclude: "node_modules/**",
-      }),
-      production && terser({ format: { comments: false } }),
     ],
+    external: Object.keys(pkg.peerDependencies || {}),
   },
 ];
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { Button } from "./components/Atoms/Button";
+import { Checkbox } from "./components/Atoms/Checkbox";
+
+export { Button, Checkbox };
