fe25048a24fbcaff420537b4677164adfe7754cc
rafael-rufino <rafael.rufino@gazin.com.br>
2022-11-22 03:16:15 -0300

    fix: ajust code

--- src/components/Atoms/Checkbox/index.tsx
+++ src/components/Atoms/Checkbox/index.tsx
@@ -1,4 +1,5 @@
 import { Checkbox as CheckboxChakra } from "@chakra-ui/react";
+import "./Checkbox.scss";
 export const Checkbox: React.FC = () => {
   return <CheckboxChakra defaultChecked>Checkbox</CheckboxChakra>;
 };
