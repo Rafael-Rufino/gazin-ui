63991d07c753453ea2ffef7be3f9a975d577c8a1
rafael-rufino <rafael.rufino@gazin.com.br>
2022-11-22 03:05:08 -0300

    fix: ajust code

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,52 @@
-import del from "rollup-plugin-delete";
-import pkg from "./package.json";
-import typescript from "@rollup/plugin-typescript";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import postcss from "rollup-plugin-postcss";
+import replace from "@rollup/plugin-replace";
+import postcssUrl from "postcss-url";
+import url from "@rollup/plugin-url";
+import alias from "@rollup/plugin-alias";
+import path from "path";
 
-export default [
-  {
-    input: "src/index.ts",
-    output: [
-      {
-        file: "dist/bundle.js",
-        format: "esm",
-        banner: "/* eslint-disable */",
-      },
-      { file: pkg.main, format: "cjs" },
-      { file: pkg.module, format: "esm" },
-    ],
-    plugins: [
-      resolve(),
-      commonjs(),
-      del({ targets: ["dist/*", "lib"] }),
-      typescript(),
-    ],
-    external: Object.keys(pkg.peerDependencies || {}),
-  },
-];
+const packageJson = require("./package.json");
+
+export default {
+  input: "src/index.ts",
+  external: ["@emotion/react", "@emotion/styled"],
+  output: [
+    {
+      file: packageJson.module,
+      format: "esm",
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    alias({
+      entries: [
+        {
+          find: "~",
+          replacement: path.resolve(__dirname, "src"),
+        },
+      ],
+    }),
+    postcss({
+      plugins: [postcssUrl({ url: "inline" })],
+      extensions: [".scss", ".css"],
+      minimize: true,
+    }),
+    url({ emitFiles: false }),
+    replace({
+      "process.env.NODE_ENV": JSON.stringify("production"),
+      preventAssignment: true,
+    }),
+    resolve({
+      skip: ["react", "react-dom", "react-router-dom"],
+    }),
+    commonjs(),
+    typescript({
+      tsconfig: "./tsconfig.json",
+      exclude: ["**/*.stories.tsx", "**/*.test.tsx", "src/helpers"],
+    }),
+  ],
+};
--- src/components/Atoms/Button/index.tsx
+++ src/components/Atoms/Button/index.tsx
@@ -1,4 +1,6 @@
 import { Button as ButtonChakra } from "@chakra-ui/react";
+import "./Button.scss";
+
 export const Button = () => {
   return (
     <ButtonChakra h="54px" colorScheme="blue" variant="solid">
